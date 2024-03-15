8adfe32724d8801f0f8247550c567c68c5cb6c67
Aleqyan777 <aleqyan.2000@mail.ru>
2024-03-15 23:13:12 +0400

    categories

--- src/App.jsx
+++ src/App.jsx
@@ -6,30 +6,29 @@ export default function App() {
   const [data,setdata] = useState([
     {
       id:1,
-      title:'Mersedes Benz',
-      price:87000,
-      image: './images/mers.jpg.jpg',
-      info : ' Daimlers ultraluxury Maybach brand was under the Mercedes-Benz Cars division until December 2012, when production was stopped due to decreased sales.27 It now exists under the Mercedes-Maybach name, with the models being luxury-focused, enhanced models of Mercedes-Benz cars, '
+      title:'HTML 5',
+      image: './images/Html.jpg.png',
+      desc: 'HTML5 includes detailed processing models to encourage more interoperable implementations; it extends, improves, and rationalizes the markup available for documents and introduces markup and application programming interfaces (APIs) for complex web applications.[8] For the same reasons, HTML5 is also a candidate for cross-platform mobile applications because it includes features designed with low-powered devices in mind.'
+     
     },
     {
       id:2,
-      title:'bmw',
-      price:97000,
-      image: './images/bmw.jpg.jpg',
-      info : 'BMW introduced anti-lock brakes on production motorcycles starting in the late 1980s. The generation of anti-lock brakes available on the 2006 and later BMW motorcycles paved the way for the introduction of electronic stability control, or anti-skid technology later in the 2007 model year.'
-
+      title:'CSS 3',
+      image: './images/Css.jpg.png',
+      desc: 'CSS is designed to enable the separation of content and presentation, including layout, colors, and fonts.[3] This separation can improve content accessibility;[further explanation needed] provide more flexibility and control in the specification of presentation characteristics; enable multiple web pages to share formatting by specifying the relevant CSS in a separate .css file, which reduces complexity and repetition in the structural content; and enable the '
+      
     },
     {
       id:3,
-      title:'Audi',
-      price:107000,
-      image: './images/audi.jpg.jpg',
-      info:'Like most German manufacturing, at the onset of World War II the Auto Union plants were retooled for military production, and were a target for allied bombing during the war which left them damaged.'
+      title:'Java Script',
+      image: './images/JavaScript.jpg',
+      desc: 'JavaScript (/ˈdʒɑːvəskrɪpt/), often abbreviated as JS, is a programming language and core technology of the World Wide Web, alongside HTML and CSS. 99% of websites use JavaScript on the client side for webpage behavior,[10] often incorporating third-party libraries. All major web browsers have a dedicated JavaScript engine to execute the code on users'
+   
     }
   ])
   return (
     <div className='App'>
-      <h1>Cars in stock</h1>
+      <h1>Categories</h1>
       <div className='App__container'>
         {
            data.map(elem =>{
@@ -39,8 +38,8 @@ export default function App() {
               <img src={elem.image} alt={elem.title} />
                <div>
                 <h2>{elem.title}</h2>
-                <p className='App__price'>${elem.price}</p>
-                <span className='App__infio'>${elem.info}</span>
+                
+                <span className='App__desc'>${elem.desc}</span>
                </div>
 
               </article>
