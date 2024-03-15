abee41a139091932d48c41dc7d2cbc8279611d15
Hovo <www.hovo099646568@gmail.com>
2024-03-15 20:56:44 +0400

    React 15.03.2024

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './React/1/App'
+import App from './React/3/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
