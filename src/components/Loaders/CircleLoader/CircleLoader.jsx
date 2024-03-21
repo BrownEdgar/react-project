5c5c7a3b9bdd097a950e48861d87c53217c653cb
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-03-21 10:53:32 +0400

    quotes

---
+++ src/components/Loaders/CircleLoader/CircleLoader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Loader from '../../assets/loader.svg'
+import './CircleLoader.scss'
+
+export default function CircleLoader() {
+  return (
+    <div className="Loader" >
+      <img src={Loader} alt="Loader" />
+    </div>
+  )
+}
--- src/components/Quotes/Quotes.jsx
+++ src/components/Quotes/Quotes.jsx
@@ -1,35 +1,72 @@
 import React, { useState, useEffect } from 'react';
+import CircleLoader from '../Loaders/CircleLoader/CircleLoader'
 import './Quotes.scss'
 
-
 export default function Quotes() {
-  const [quote, setQuote] = useState({});
-  const [quoteNumber, setQoteNumber] = useState(1)
+  const [quote, setQuote] = useState([]);
+  const [quoteNumber, setQoteNumber] = useState(0)
+  const [loading, setLoading] = useState(true)
 
   useEffect(() => {
-
-    fetch(`https://dummyjson.com/quotes/${quoteNumber}`)
+    setLoading(true)
+    fetch(`https://dummyjson.com/quotes`)
       .then(res => res.json())
-      .then(data => setQuote(data));
+      .then(data => setQuote(data.quotes))
+      .finally(() => setLoading(false))
+  }, [])
 
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(setLoading, 300, false)
   }, [quoteNumber])
 
-  const next = () => setQoteNumber(quoteNumber < 100 ? quoteNumber + 1 : 100)
-  const prev = () => setQoteNumber(quoteNumber > 2 ? quoteNumber - 1 : 1)
+
+  const handleLike = (quoteId) => {
+    const result = quote.map(elem => {
+      if (elem.id === quoteId) {
+        elem.like = !elem.like;
+      }
+      return elem;
+    })
+    setQuote(result)
+  }
+
+  const next = () => setQoteNumber(quoteNumber < 29 ? quoteNumber + 1 : 29)
+  const prev = () => setQoteNumber(quoteNumber > 1 ? quoteNumber - 1 : 1)
+  const currentQoute = quote[quoteNumber];
 
   return (
     <div className='Quotes'>
       <h1>Quotes</h1>
       <div className="Quotes__item">
-        <h1>{quote.quote}</h1>
-        <h3>{quote.author}</h3>
+        {
+          loading ? (
+            <CircleLoader />
+          ) : (
+            <>
+              <h1 className={`${currentQoute?.quote.length > 85 ? 'Quotes__sm' : ''}`}>{currentQoute.quote}</h1>
+              <h3 className='Quotes__author'>{currentQoute.author}</h3>
+              <span
+                className='Quotes__icon Quotes__icon-like'
+                onClick={() => handleLike(quote[quoteNumber].id)}>
+                {
+                  (quote[quoteNumber].like)
+                    ? <i className="bi bi-hand-thumbs-up-fill"></i>
+                    : <i className="bi bi-hand-thumbs-up"></i>
+                }
+              </span>
+            </>
+          )
+        }
+
+
       </div>
       <div className="Quotes__buttons">
-        <button>
-          <i className="bi bi-arrow-left" onClick={prev}></i>
+        <button onClick={prev}>
+          <i className="bi bi-arrow-left"></i>
         </button>
-        <button>
-          <i className="bi bi-arrow-right" onClick={next}></i>
+        <button onClick={next}>
+          <i className="bi bi-arrow-right" ></i>
         </button>
       </div>
     </div>
