b65a0fdf97bb7971e9c3dacf23bf34caefeeba76
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-03-23 11:11:29 +0400

    fix code

--- src/React/6/App.jsx
+++ src/React/6/App.jsx
@@ -3,39 +3,38 @@ import './App.scss';
 import Pagination from '../compponents/pagenation/Pagenation.jsx';
 
 export default function App() {
-    const [posts, setPosts] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [page, setPage] = useState(1);
-    const [perPage, setPerPage] = useState(1);
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [perPage, setPerPage] = useState(1);
 
-    useEffect(() => {
-        setLoading(true);
-        fetch(`https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${perPage}`)
-            .then(res => res.json())
-            .then(data => { setPosts(data); })
-            .finally(() => setLoading(false));
-    }, [page, perPage]);
+  useEffect(() => {
+    setLoading(true);
+    fetch(`https://jsonplaceholder.typicode.com/posts?_start=${page}&_limit=${perPage}`)
+      .then(res => res.json())
+      .then(data => setPosts(data))
+      .finally(() => setLoading(false));
+  }, [page, perPage]);
 
-    const firstIndex = (page - 1) * perPage;
-    const firstPage = posts.slice(firstIndex, firstIndex + perPage);
-    const setPages =(n)=>{setPage(n)}
-    
 
-    return (
-        <div className='Container'>
-            {loading ? (
-                <div>Loading...</div>
-            ) : (
-                <div className='Container__block'>
-                    {firstPage.map(post => (
-                        <div key={post.id} className='Container__block__posts'>
-                            <h1>{post.id}. {post.title}</h1>
-                            <p>{post.body}</p>
-                        </div>
-                    ))}
-                </div>
-            )}
-            <Pagination total={100} perPage={perPage} setPages={setPages}/> 
+  const setPages = (n) => { setPage(n) }
+
+
+  return (
+    <div className='Container'>
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
+        <div className='Container__block'>
+          {posts.map(post => (
+            <div key={post.id} className='Container__block__posts'>
+              <h1>{post.id}. {post.title}</h1>
+              <p>{post.body}</p>
+            </div>
+          ))}
         </div>
-    );
+      )}
+      <Pagination total={100} perPage={perPage} setPages={setPages} />
+    </div>
+  );
 }
