7cf3551a8ef82fecc6a815d31c07a4a336e495c5
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-03-28 11:09:56 +0400

    fix

--- src/UI/Title/Button/Button.jsx
+++ src/UI/Title/Button/Button.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import './Button.scss'
-export default function Button({ as = 'button', title = "No title", classList, size, form, align }) {
-    const Element = as;
-    return (
-        <Element className={`custom-btn btn-10${classList ? classList : ''} Button-${form}`}>
-            {title}
-        </Element>
-    )
+export default function Button({ as = 'button', title = "No title", classList, size, form, align, onclick }) {
+  const Element = as;
+  return (
+    <Element className={`custom-btn btn-10 ${classList ? classList : ''} Button-${form}`}
+      onClick={onclick}>
+      {title}
+    </Element>
+  )
 }
