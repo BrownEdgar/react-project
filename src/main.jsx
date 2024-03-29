606d006827a32d5ac63470c3ba22466aee70e40e
Aleqyan777 <aleqyan.2000@mail.ru>
2024-03-30 00:16:13 +0400

    json serverrrr

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-<<<<<<< HEAD
 import App from './lessons/json-server/App'
-=======
-import App from './HW/App'
->>>>>>> 9e9e5dc6d6dc62692170de0dfd16b3687e31318e
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
