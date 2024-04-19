e6f63884c8b10bf66e7fa3286d2ecc79d9e7d57f
Sirak-Harutyunyan <sirakharutyunyan47@gmail.com>
2024-04-19 22:27:18 +0400

    custom hook

--- src/Home Worck/hooks/useCustom/Custome.jsx
+++ src/Home Worck/hooks/useCustom/Custome.jsx
@@ -12,7 +12,7 @@ const [code , {generateCode}] = usePassword(15)
   return (
     <div className='Custome'> 
     
-        <h1>Count: {JSON.stringify(code)}</h1>
+        <h1>Password: {JSON.stringify(code)}</h1>
         <button onClick={generateCode}>code</button>
         <hr/>
         <h1>Count: {count}</h1>
