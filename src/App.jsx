32a7b0f889ee17774933d43382e491b566cbc220
BrownEdgar <nikoghosjanedgar@gmail.com>
2024-04-23 12:45:48 +0400

    redux part 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { useSelector, useDispatch, } from 'react-redux'
+import { addCounter } from './features/counter/counterSlice'
 
 export default function App() {
+
+  const counter = useSelector(state => state.counter)
+  const dispatch = useDispatch()
+
   return (
-    <div>App</div>
+    <div>
+      <h1>counter: {counter}</h1>
+      <button onClick={() => dispatch(addCounter())}>Add counter</button>
+    </div>
   )
 }
+
---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+
+const counterSlice = createSlice({
+  name: 'counter',
+  initialState: 0,
+  reducers: {
+    addCounter(state) {
+      return state + 1;
+    }
+  }
+})
+
+export default counterSlice.reducer;
+export const { addCounter } = counterSlice.actions
