88cd48c627c2420adad93088a4e14ffae1b67993
Sirak-Harutyunyan <sirakharutyunyan47@gmail.com>
2024-04-25 00:02:11 +0400

    redux 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,16 @@
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
-import DevCategories from './Home Worck/DevCategories/DevCategories'
-import FData from './Home Worck/FData/FData'
-import OurProducts from './Home Worck/OurProducts/OurProducts'
-import Users from './Home Worck/Users/Users'
-import Pagination from './Home Worck/Pagination/Pagination'
-import AxiosTask from './Home Worck/Axios/AxiosTask'
-import Server from './Home Worck/json-server/Server'
-// menu
-import {Home,Blog,Bloger,PlaceBloger,About,Posts,Post,ErrorPage} from './MainMenu/Pages/imports'
-import { Routes, Route } from 'react-router-dom'
-import MainLayouts from './MainMenu/NavBar/Layouts/MainLayouts'
-
-import User from './Home Worck/UserAP/User'
-import Custome from './Home Worck/hooks/useCustom/Custome'
-import AppContext from './Home Worck/hooks/useContext/App'
-
-
+import { increment,decrement} from './features/counter/counterSlice'
 
 export default function App() {
+  const state = useSelector((state) => state.counter)
+  const dispatch = useDispatch()
   return (
     <div className='App'>
-      {/* <FData /> */}
-      {/* <DevCategories /> */}
-      {/* <Users/> */}
-      {/* <OurProducts/> */}
-      {/* <Pagination/> */}
-      {/* <PaginateBlog/> */}
-      {/* <AxiosTask/> */}
-      {/* <Server/> */}
-
-      {/* <Routes>
-        <Route path='/' element={<MainLayouts/>}>
-        <Route index element={<Home />} />
-        <Route path='blog' element={<Blog />} />
-        <Route path='blog/:id' element={<Bloger />} />
-        <Route path='blog/place/:id' element={<PlaceBloger />} />
-        <Route path='about' element={<About />} />
-        <Route path='posts' element={<Posts />} />
-        <Route path='posts/:id' element={<Post />} />
-        <Route path='*' element={<ErrorPage />} />
-        </Route>
-      </Routes> */}
-
-      {/* <User/> */}
-      {/* <Custome/> */}
-      <AppContext/>
-
-
+      <h3>counter : {state}</h3>
+      <button onClick={() => dispatch(increment())}>increment</button>
+      <button onClick={() => dispatch(decrement())}>decrement</button>
     </div>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './Home Worck/hooks/useContext/App'
+import App from './App'
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 import './index.css'
-import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <Router>
+  <Provider store ={store}>
     <App />
-    </Router>
-  </React.StrictMode>,
+  </Provider>
 )
