ca0d4867900b620fea65e15991527a18b6660ff8
Sirak-Harutyunyan <sirakharutyunyan47@gmail.com>
2024-04-27 01:10:52 +0400

    extraReducers

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import './App.css'
-import { increment,decrement} from './features/counter/counterSlice'
+import Todos from './components/Todos/Todos'
+import { increment,decrement, getCounter} from './features/counter/counterSlice'
+import { getAsyncTodos } from './features/todos/todosSlice'
 
 export default function App() {
-  const state = useSelector((state) => state.counter)
+  const state = useSelector(getCounter)
   const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(getAsyncTodos())
+  }, [])
+
   return (
     <div className='App'>
       <h3>counter : {state}</h3>
       <button onClick={() => dispatch(increment())}>increment</button>
       <button onClick={() => dispatch(decrement())}>decrement</button>
+      <hr/>
+      <Todos/>
     </div>
   )
 }
--- src/features/counter/counterSlice.js
+++ src/features/counter/counterSlice.js
@@ -11,8 +11,12 @@ export const counterSlice = createSlice({
         decrement: (state) => {
             return state - 1
         }
+    },
+    selectors:{
+        getCounter:(state)=>state
     }
 })
 
 export const { increment,decrement } = counterSlice.actions
+export const { getCounter } = counterSlice.selectors
 export default counterSlice.reducer
