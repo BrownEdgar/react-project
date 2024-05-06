2d1047e6a066808038cbac3698da4975c0097b19
Viliam <vil.ghazaryan@gmail.com>
2024-05-06 07:16:55 -0800

    change Letter using state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
-
+import { useState } from "react";
+import './App.css';
 export default function App() {
-  return (
-    <div>App</div>
-  )
+    const [text, setText] = useState("...");
+
+    return (<>
+        <div className="main-wrapper">
+            <div className="btns-wrapper">
+                <button onClick={((event) => setText(event.target.textContent))}>A</button>
+                <button onClick={((e) => setText(e.target.textContent))}>B</button>
+                <button onClick={((e) => setText(e.target.textContent))}>C</button>
+                <button onClick={((e) => setText(e.target.textContent))}>D</button>
+            </div>
+            <div className="result-wrapper">
+                <div className="result">{text}</div>
+            </div>
+        </div>
+    </>)
 }
